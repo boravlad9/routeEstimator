cbf5f79d600957250d995be46793252583f9a058
Bora Vlad <bora.li.vlad@utcluj.didatec.ro>
2022-04-17 22:43:56 +0300

    update

--- src/main/java/code/roadEstimator/services/FileInitializer.java
+++ src/main/java/code/roadEstimator/services/FileInitializer.java
@@ -38,7 +38,8 @@ public class FileInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         try {
-            URL resource = ClassLoader.getSystemResource("RoutesEvaluation_log.txt");
+            URL resource = FileInitializer.class.getClassLoader().getSystemResource("RoutesEvaluation_log.txt");
+            System.out.println(resource.toURI());
             File myObj = new File(resource.toURI());
             Scanner myReader = new Scanner(myObj);
             List<Pattern> ignoredPatterns = new ArrayList<>();
